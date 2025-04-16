373cfcf709506e58c48b02ef634e08cf518a2d1d
coyuki <fu08yu20ko07@icloud.com>
2025-04-16 09:00:50 +0900

    refc

--- src/lib.rs
+++ src/lib.rs
@@ -47,29 +47,7 @@ impl Issue {
             body_path: body_path.as_ref().to_path_buf(),
         }
     }
-    // pub fn add(new_issue: Self) -> Result<(), Error> {
-    //     let data = sled::open(db_path()?).map_err(Error::DbError)?;
-    //     let serialized_data = bincode::serialize(&new_issue).map_err(Error::BinError)?;
-    //     let _result = data
-    //         .insert(&new_issue.title, serialized_data)
-    //         .map_err(Error::DbError)?;
-    //     Ok(())
-    // }
-    // // fn load(target: String) -> Result<(), Error> {
-    // //     let data = sled::open(db_path()?).map_err(Error::DbError)?;
-    // //     data.get(key)
-    // // }
-    // pub fn get_all() -> Result<(), Error> {
-    //     let db = sled::open(db_path()?).map_err(Error::DbError)?;
-    //     let _a = db.get("title").map_err(Error::DbError)?;
-    //     Ok(())
-    // }
 }
-
-/// for test, lib users don't use this.
-// fn db_path() -> Result<PathBuf, Error> {
-//     Ok(PathBuf::from("value"))
-// }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Project {
@@ -116,7 +94,7 @@ impl Project {
 
         let mut match_type = MatchType::Exact;
         // TODO: mutを消せるように
-        // ブラケットで囲んでifで分岐すればおそらく束縛は1回で済む
+        // ブラケットで囲んでifで分岐すれば、おそらく束縛は1回で済む
         let mut found_issues: Vec<Issue> = loaded_db
             .body
             .iter()
@@ -159,29 +137,4 @@ pub enum Status {
 
 #[cfg(test)]
 mod tests {
-    use crate::{Issue, Project};
-
-    const WORKPATCH: &str = "/Users/yuki/Develop/local_issues_lib/";
-
-    #[test]
-    fn project_init_test() {
-        let result = Project::open("title", WORKPATCH);
-        assert!(result.is_ok());
-    }
-
-    #[test]
-    fn add_new_issue_test() {
-        let pro = Project::open("title", WORKPATCH).unwrap();
-        println!("{:?}", pro);
-        let new_issue = Issue::new("title", None, crate::Status::Open, "body_path");
-        let result = pro.add(new_issue);
-        println!("{:?}", result);
-    }
-
-    #[test]
-    fn load_test() {
-        let pro = Project::open("title", WORKPATCH).unwrap();
-        let loaded = pro.load();
-        println!("{:?}", loaded);
-    }
 }
